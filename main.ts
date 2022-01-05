dc11f34f0b51aadc9d111d1970a78fa40ec98f2d
montsp <78172324+montsp@users.noreply.github.com>
2022-01-05 10:33:17 +0100

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-radio.onReceivedString(function (receivedString) {
-    明るさ = receivedString
+radio.onReceivedNumber(function (receivedNumber) {
+    明るさ = receivedNumber
 })
 let 明るさ = 0
 radio.setGroup(237)
